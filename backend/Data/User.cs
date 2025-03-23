fdfe96ddedf37cb81f19dda600161064ef09660e
Przewodzik <lukasz_prze@wp.pl>
2025-03-23 16:13:29 +0100

    merge z Grupson_project

---
+++ backend/Data/User.cs
@@ -0,0 +1,12 @@
+namespace backend.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool EmailConfirmed { get; set; } = false;
+        public string? ConfirmationToken { get; set; }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
@@ -8,11 +12,33 @@ builder.Services.AddCors(options =>
                         .AllowAnyHeader());
 });
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var emailSettings = builder.Configuration.GetSection("EmailSettings").Get<EmailSettings>();
+builder.Services.AddSingleton(emailSettings);
 builder.Services.AddControllers();
+builder.Services.AddScoped<EmailService>();
+
 
 var app = builder.Build();
 
 app.UseCors("AllowFrontend");
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+// Dodaj użytkownika tylko jeśli baza jest pusta
+if (!context.Users.Any())
+{
+    
+    context.Users.Add(new User { Name = "JanKowalski", Email = "jan@example.com", Password = BCrypt.Net.BCrypt.HashPassword("jankowalski123") });
+    context.Users.Add(new User { Name = "AnnaNowak", Email = "anna@example.com", Password = BCrypt.Net.BCrypt.HashPassword("annanowak987") });
+
+    context.SaveChanges();
+    Console.WriteLine("Dodano użytkowników do bazy SQLite!");
+}
 
 app.Run();
