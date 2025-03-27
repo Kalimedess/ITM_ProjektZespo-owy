224f9e6fe49c51a57e2686bd31e1d9d2853c20a1
MateuszMiller <115210753+MateuszMiller@users.noreply.github.com>
2025-03-28 00:27:50 +0100

    Added tree image zoom

--- frontend/src/views/cheatSheetView.vue
+++ frontend/src/views/cheatSheetView.vue
@@ -1,10 +1,25 @@
 <template>
-    <div class="w-full h-full flex-1 flex flex-col justify-center items-center text-center text-white" >
-      <h1 class="m-8 font-nasalization text-7xl">Drzewo decyzji</h1>
-      <img :src="drzewko" class="w-100 border-2 border-lgray-accent p-2 overflow-hidden rounded-md" alt="ITM logo" >
-    </div>
-  </template>
-  
-  <script setup>
-  import drzewko from '@/assets/viewPNGs/DrzewoDecyzji.png'
-  </script>
+  <div class="w-full h-full flex-1 flex flex-col justify-center items-center text-center text-white">
+    <h1 class="m-8 font-nasalization text-7xl">Drzewo decyzji</h1>
+    <img
+      :src="drzewko"
+      :style="{ width: imageWidth, maxWidth: '100%' }"
+      class="w-100 border-2 border-lgray-accent p-2 overflow-hidden rounded-md transition-all duration-300 cursor-pointer"
+      alt="ITM logo"
+      @click="toggleImageSize"
+    >
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import drzewko from '@/assets/viewPNGs/DrzewoDecyzji.png'
+
+const isZoomed = ref(false)
+const imageWidth = ref('300px')
+
+function toggleImageSize() {
+  isZoomed.value = !isZoomed.value
+  imageWidth.value = isZoomed.value ? '800px' : '300px'
+}
+</script>
