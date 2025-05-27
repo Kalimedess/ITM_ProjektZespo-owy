2c53c1447a2e2d2871a6473668c9314acce34034
Przewodzik <lukasz_prze@wp.pl>
2025-05-27 20:45:58 +0200

    Poprawiono pionek

--- frontend/src/components/game/gameBoard.vue
+++ frontend/src/components/game/gameBoard.vue
@@ -264,30 +264,55 @@ for (let i = 0; i < props.config.Rows; i += 2) {
 
     // Rysowanie pionka
     if (props.gameMode) {
-      svg.append("circle")
-        .attr("class", "pawn")
-        .attr("cx", props.posX * cellSize.value + marginLeft.value + cellSize.value / 2)
-        .attr("cy", props.posY * cellSize.value + marginTop.value + cellSize.value / 2)
-        .attr("r", cellSize.value * 0.3)
-        .attr("fill", props.pawnColor);
-    }
 
+      const centerX = props.posX * cellSize.value + marginLeft.value + cellSize.value / 2;
+      const centerY = props.posY * cellSize.value + marginTop.value + cellSize.value / 2;
+
+      const pawnPath = "M 225.5,294.5 C 231.979,295.491 238.646,295.824 245.5,295.5C 245.5,311.167 245.5,326.833 245.5,342.5C 179.833,342.5 114.167,342.5 48.5,342.5C 48.5,326.5 48.5,310.5 48.5,294.5C 55.1667,294.5 61.8333,294.5 68.5,294.5C 68.8256,290.116 68.4922,285.783 67.5,281.5C 59.5866,274.592 56.7533,265.925 59,255.5C 62.8813,244.72 70.548,238.72 82,237.5C 104.247,207.195 115.413,173.195 115.5,135.5C 92.123,118.375 84.2897,95.7084 92,67.5C 105.287,36.9129 128.453,24.4129 161.5,30C 194.896,41.2769 209.063,64.4436 204,99.5C 200.388,115.436 191.722,127.769 178,136.5C 178.299,166.031 185.633,193.698 200,219.5C 204.448,226.282 209.281,232.782 214.5,239C 235.289,244.415 241.123,256.915 232,276.5C 226.213,280.998 224.047,286.998 225.5,294.5 Z";
+      
+      const scale = cellSize.value * 0.002;
+      
+      const originalCenterX = 147;
+      const originalCenterY = 186; 
+      
+      svg.append("path")
+        .attr("class", "pawn")
+        .attr("d", pawnPath)
+        .attr("fill", props.pawnColor)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1.2 / scale)
+        .attr("transform", `
+          translate(${centerX}, ${centerY})
+          scale(${scale})
+          translate(${-originalCenterX}, ${-originalCenterY})
+        `);
+    }
   };
 
-  //Animacje pionka 
-    const animatePawn = () => {
-      
-      const pawn = d3.select(board.value).select(".pawn");
-
-      pawn.transition()
-        .duration(500)
-        .attr("cx", props.posX * cellSize.value + marginLeft.value + cellSize.value / 2)
-        .attr("cy", props.posY * cellSize.value + marginTop.value + cellSize.value / 2)
-        .on("end", () => {
-          currentPosX.value = props.posX;
-          currentPosY.value = props.posY;
-        });
-    }
+  //Animacja pionka
+  const animatePawn = () => {
+  const pawn = d3.select(board.value).select(".pawn");
+  
+  const newX = props.posX * cellSize.value + marginLeft.value + cellSize.value / 2;
+  const newY = props.posY * cellSize.value + marginTop.value + cellSize.value / 2;
+  
+  const scale = cellSize.value * 0.002;
+  const originalCenterX = 147;
+  const originalCenterY = 186;
+  
+  pawn.transition()
+    .duration(500)
+    .ease(d3.easeCubicInOut)
+    .attr("transform", `
+      translate(${newX}, ${newY})
+      scale(${scale})
+      translate(${-originalCenterX}, ${-originalCenterY})
+    `)
+    .on("end", () => {
+      currentPosX.value = props.posX;
+      currentPosY.value = props.posY;
+    });
+}
 
     // Obserwowanie zmian w konfiguracji planszy
     watch(() => props.config, () => {
