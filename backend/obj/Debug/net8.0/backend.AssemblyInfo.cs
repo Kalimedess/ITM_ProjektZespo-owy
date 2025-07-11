1580c2ba34689e2437be8c264619352dcad2ac1a
Diper <makros123456@gmail.com>
2025-07-11 17:34:34 +0200

    do usunięcie

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1987af146f90cee5130673281b6dd3a62ee281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58fa2bae53e35da96603e382878b97b4851eebc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
